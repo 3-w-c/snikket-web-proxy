bcec18a66f5a61aace1d2a646afda5bbf2de9b79
3wc <3wc.github@doesthisthing.work>
2021-10-31 16:49:43 +0200

    Further bag o' hacks for configurable SSL

--- cert-monitor.sh
+++ cert-monitor.sh
@@ -4,7 +4,8 @@ if test -f /etc/nginx/sites-enabled/startup; then
 	rm /etc/nginx/sites-enabled/startup;
 fi
 /usr/local/bin/render-template.sh "/etc/nginx/templates/snikket-common" "/etc/nginx/snippets/snikket-common.conf"
-for proto in http https; do
-	/usr/local/bin/render-template.sh "/etc/nginx/templates/$proto" "/etc/nginx/sites-enabled/$proto";
-done
+proto=http 
+/usr/local/bin/render-template.sh "/etc/nginx/templates/$proto" "/etc/nginx/sites-enabled/$proto";
 /usr/sbin/nginx -s reload
+
+sleep inf
